7e6684a9e3479c5ac0eff3587ba9f6e139a54e07
Josh Levy <joshaaronlevy@gmail.com>
2019-10-25 06:39:32 -0600

    "Got the header and sidenav good enough to move forward"

--- src/app/shared/navbar/navbar.component.ts
+++ src/app/shared/navbar/navbar.component.ts
@@ -1,15 +1,16 @@
-import { Component, OnInit, Renderer, ViewChild, ElementRef, Directive } from '@angular/core';
+import { Component, OnInit, Renderer, ViewChild, ElementRef, Directive, Renderer2 } from '@angular/core';
 import { ROUTES } from '../.././sidebar/sidebar.component';
 import { Router, ActivatedRoute, NavigationEnd, NavigationStart } from '@angular/router';
 import { Subscription } from 'rxjs/Subscription';
 import { Location, LocationStrategy, PathLocationStrategy } from '@angular/common';
+
 const misc: any = {
     navbar_menu_visible: 0,
     active_collapse: true,
     disabled_collapse_init: 0,
 };
 
-declare var $: any;
+declare let $: any;
 @Component({
     selector: 'app-navbar-cmp',
     templateUrl: 'navbar.component.html'
@@ -26,37 +27,41 @@ export class NavbarComponent implements OnInit {
 
     @ViewChild('app-navbar-cmp', {static: false}) button: any;
 
-    constructor(location: Location, private renderer: Renderer, private element: ElementRef, private router: Router) {
-        this.location = location;
-        this.nativeElement = element.nativeElement;
-        this.sidebarVisible = false;
+    constructor(
+      location: Location,
+      private renderer: Renderer2,
+      private element: ElementRef,
+      private router: Router
+    ) {
+      this.location = location;
+      this.nativeElement = element.nativeElement;
+      this.sidebarVisible = false;
     }
-    minimizeSidebar(){
+
+    minimizeSidebar() {
       const body = document.getElementsByTagName('body')[0];
-
       if (misc.sidebar_mini_active === true) {
-          body.classList.remove('sidebar-mini');
-          misc.sidebar_mini_active = false;
-
+        body.classList.remove('sidebar-mini');
+        misc.sidebar_mini_active = false;
       } else {
-          setTimeout(function() {
-              body.classList.add('sidebar-mini');
+        setTimeout(function() {
+          body.classList.add('sidebar-mini');
 
-              misc.sidebar_mini_active = true;
-          }, 300);
+          misc.sidebar_mini_active = true;
+        }, 300);
       }
 
       // we simulate the window Resize so the charts will get updated in realtime.
       const simulateWindowResize = setInterval(function() {
-          window.dispatchEvent(new Event('resize'));
+        window.dispatchEvent(new Event('resize'));
       }, 180);
 
       // we stop the simulation of Window Resize after the animations are completed
       setTimeout(function() {
-          clearInterval(simulateWindowResize);
+        clearInterval(simulateWindowResize);
       }, 1000);
     }
-    hideSidebar(){
+    hideSidebar() {
       const body = document.getElementsByTagName('body')[0];
       const sidebar = document.getElementsByClassName('sidebar')[0];
 
@@ -96,11 +101,13 @@ export class NavbarComponent implements OnInit {
         const body = document.getElementsByTagName('body')[0];
         this.toggleButton = navbar.getElementsByClassName('navbar-toggler')[0];
         if (body.classList.contains('sidebar-mini')) {
-            misc.sidebar_mini_active = true;
+          misc.sidebar_mini_active = true;
         }
+
         if (body.classList.contains('hide-sidebar')) {
-            misc.hide_sidebar_active = true;
+          misc.hide_sidebar_active = true;
         }
+
         this._router = this.router.events.filter(event => event instanceof NavigationEnd).subscribe((event: NavigationEnd) => {
           this.sidebarClose();
 
@@ -110,97 +117,96 @@ export class NavbarComponent implements OnInit {
           }
         });
     }
+
     onResize(event) {
       if ($(window).width() > 991) {
         return false;
       }
       return true;
     }
+
     sidebarOpen() {
-      var $toggle = document.getElementsByClassName('navbar-toggler')[0];
-        const toggleButton = this.toggleButton;
-        const body = document.getElementsByTagName('body')[0];
-        setTimeout(function(){
-            toggleButton.classList.add('toggled');
-        }, 500);
-        body.classList.add('nav-open');
-        setTimeout(function() {
-            $toggle.classList.add('toggled');
-        }, 430);
+      let $toggle = document.getElementsByClassName('navbar-toggler')[0];
+      const toggleButton = this.toggleButton;
+      const body = document.getElementsByTagName('body')[0];
+      setTimeout(function() {
+        toggleButton.classList.add('toggled');
+      }, 500);
+      body.classList.add('nav-open');
+      setTimeout(function() {
+        $toggle.classList.add('toggled');
+      }, 430);
 
-        var $layer = document.createElement('div');
-        $layer.setAttribute('class', 'close-layer');
+      let $layer = document.createElement('div');
+      $layer.setAttribute('class', 'close-layer');
 
+      if (body.querySelectorAll('.main-panel')) {
+        document.getElementsByClassName('main-panel')[0].appendChild($layer);
+      } else if (body.classList.contains('off-canvas-sidebar')) {
+        document.getElementsByClassName('wrapper-full-page')[0].appendChild($layer);
+      }
 
-        if (body.querySelectorAll('.main-panel')) {
-            document.getElementsByClassName('main-panel')[0].appendChild($layer);
-        }else if (body.classList.contains('off-canvas-sidebar')) {
-            document.getElementsByClassName('wrapper-full-page')[0].appendChild($layer);
-        }
+      setTimeout(function() {
+        $layer.classList.add('visible');
+      }, 100);
 
-        setTimeout(function() {
-            $layer.classList.add('visible');
-        }, 100);
-
-        $layer.onclick = function() { //asign a function
-          body.classList.remove('nav-open');
-          this.mobile_menu_visible = 0;
-          this.sidebarVisible = false;
-
-          $layer.classList.remove('visible');
-          setTimeout(function() {
-              $layer.remove();
-              $toggle.classList.remove('toggled');
-          }, 400);
-        }.bind(this);
-
-        body.classList.add('nav-open');
-        this.mobile_menu_visible = 1;
-        this.sidebarVisible = true;
-    };
-    sidebarClose() {
-      var $toggle = document.getElementsByClassName('navbar-toggler')[0];
-        const body = document.getElementsByTagName('body')[0];
-        this.toggleButton.classList.remove('toggled');
-        var $layer = document.createElement('div');
-        $layer.setAttribute('class', 'close-layer');
-
+      $layer.onclick = function() {
+        body.classList.remove('nav-open');
+        this.mobile_menu_visible = 0;
         this.sidebarVisible = false;
-        body.classList.remove('nav-open');
-        // $('html').removeClass('nav-open');
-        body.classList.remove('nav-open');
-        if ($layer) {
-            $layer.remove();
-        }
 
+        $layer.classList.remove('visible');
         setTimeout(function() {
+            $layer.remove();
             $toggle.classList.remove('toggled');
         }, 400);
+      }.bind(this);
 
-        this.mobile_menu_visible = 0;
-    };
+      body.classList.add('nav-open');
+      this.mobile_menu_visible = 1;
+      this.sidebarVisible = true;
+    }
+
+    sidebarClose() {
+      let $toggle = document.getElementsByClassName('navbar-toggler')[0];
+      const body = document.getElementsByTagName('body')[0];
+      this.toggleButton.classList.remove('toggled');
+      let $layer = document.createElement('div');
+      $layer.setAttribute('class', 'close-layer');
+
+      this.sidebarVisible = false;
+      body.classList.remove('nav-open');
+      body.classList.remove('nav-open');
+      if ($layer) {
+        $layer.remove();
+      }
+
+      setTimeout(function() {
+        $toggle.classList.remove('toggled');
+      }, 400);
+
+      this.mobile_menu_visible = 0;
+    }
     sidebarToggle() {
-        if (this.sidebarVisible === false) {
-            this.sidebarOpen();
-        } else {
-            this.sidebarClose();
-        }
+      if (this.sidebarVisible === false) {
+        this.sidebarOpen();
+      } else {
+        this.sidebarClose();
+      }
     }
 
     getTitle() {
-      var titlee = this.location.prepareExternalUrl(this.location.path());
-      if(titlee.charAt(0) === '#'){
-          titlee = titlee.slice( 1 );
+      let title = this.location.prepareExternalUrl(this.location.path());
+      if(title.charAt(0) === '#') {
+          title = title.slice( 1 );
       }
         for (let i = 0; i < this.listTitles.length; i++) {
-            if (this.listTitles[i].type === "link" && this.listTitles[i].path === titlee) {
+            if (this.listTitles[i].type === 'link' && this.listTitles[i].path === title) {
                 return this.listTitles[i].title;
-            } else if (this.listTitles[i].type === "sub") {
+            } else if (this.listTitles[i].type === 'sub') {
                 for (let j = 0; j < this.listTitles[i].children.length; j++) {
                     let subtitle = this.listTitles[i].path + '/' + this.listTitles[i].children[j].path;
-                    // console.log(subtitle)
-                    // console.log(titlee)
-                    if (subtitle === titlee) {
+                    if (subtitle === title) {
                         return this.listTitles[i].children[j].title;
                     }
                 }
